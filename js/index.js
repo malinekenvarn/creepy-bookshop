7a30419c6c4b610f8a40710cfbb617734deeb89d
Malin Ekenvärn <malin.ekenvarn@gmail.com>
2022-03-02 15:50:45 +0100

    solved this bug when slides animation wont work

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+
+
 /*A script that makes a function that shows the navigation links when clicking the hamburger menu link on a mobile version*/
 
 const hamburgerButton = document.getElementsByClassName('hamburger-button')[0];
@@ -15,6 +17,7 @@ searchIcon.addEventListener('click', ()=>{
   search.classList.toggle('active');
 })
 
+
 /* Function to show a form when clicking the button to confirm order and checkout*/
 const checkout = document.getElementById('checkout-button');
 const checkoutForm = document.getElementsByClassName('checkoutForm')[0];
@@ -25,22 +28,4 @@ checkout.addEventListener('click', ()=>{
 
 
 
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (let i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
-}
 
